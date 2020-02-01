e92c1e4685450089f5fc40f99e6066efe22b65cc
Erik Chatterley-Diaz <erik@Matts-Air.lan>
2020-02-01 15:42:51 +0000

    Created main script

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def main():
+    print("***********")
+    print("Welcome to Erik's Bistro game")
+    print("***********")
+    print("");
+    username = input("What is your name? ")
+    print(f"Hello, {username}")
+
+if __name__ == '__main__':
+    main()
