ff2a05b0648072e8f05b7cf4312e8e2c2a40238e
Erik Chatterley-Diaz <erik@Matts-Air.lan>
2020-02-08 17:55:43 +0000

    Add bistro name

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ def main():
     print("Welcome to Erik's Bistro game")
     print("*****************************")
     print("");
+
     username = input("What is your name? ")
     print(f"Hello, {username}")
+
+    bistroName = input("What is your Bistro called? ")
+    print(f"Great, Welcome to {bistroName}, {username}!")
 
 if __name__ == '__main__':
     main()
